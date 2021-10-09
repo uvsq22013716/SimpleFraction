d4c27420be5ce76a20bcd588233728bd6e4410b7
Bah <saikouluqmanbah@gmail.com>
2021-10-09 17:43:08 +0200

    creation du constructeur de la classe Fraction et la generation de la methode tostring

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,18 @@
 
 public class Fraction {
+	protected int numerateur;
+	protected int denominateur;
+	
+	public Fraction(int numerateur, int denominateur)
+	{
+		this.numerateur = numerateur;
+		this.denominateur = denominateur;
+	}
 
+	@Override
+	public String toString() {
+		return "Fraction [numerateur=" + numerateur + ", denominateur=" + denominateur + "]";
+	}
+	
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
-		System.out.println("tout va bien");
+		Fraction fraction = new Fraction(5,3);
+		System.out.println(fraction);
 	}
 
 }
